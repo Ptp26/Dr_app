77cda43db524507e26067d7e8e03665e941e1d69
Ptp26 <71855186+Ptp26@users.noreply.github.com>
2020-11-28 21:11:35 -0500

    initial commit

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-
+echo "Hell0!!!";
 ?>
-<a href="View.php">Doctor</a>
-<br>
-<a href="dr_schedule/View.php">Doctor Schedule</a>
+<!--<a href="View.php">Doctor</a>-->
+<!--<br>-->
+<!--<a href="dr_schedule/View.php">Doctor Schedule</a>-->
