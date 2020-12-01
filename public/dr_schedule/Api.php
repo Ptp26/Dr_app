0e2b38d8f5f67bf49571df9811d3673a95e6b171
Ptp26 <71855186+Ptp26@users.noreply.github.com>
2020-11-30 19:16:55 -0500

    Add files via upload

---
+++ public/dr_schedule/Api.php
@@ -0,0 +1,38 @@
+<?php
+    include 'config.php';
+    $Page = $_GET["Page"];
+    //page = 1 = create
+    //page = 2 = edit
+    if($Page=="1"){
+        $name = $_GET["name"];
+
+        $sql = "INSERT INTO dr_schedule (Name) VALUES ('$name')";
+  
+        if ($mysqli->query($sql) === TRUE) {
+            header("Location: view.php");
+        } else {
+            echo "Error: " . $sql . "<br>" . $mysqli->error;
+        }         
+    }
+    elseif ($Page=="2"){
+        $name = $_GET["name"];
+        $id = $_GET["id"];
+        $sql = "UPDATE dr_schedule SET Name = '$name' WHERE ID= $id";
+  
+        if ($mysqli->query($sql) === TRUE) {
+            header("Location: view.php");
+        } else {
+            echo "Error: " . $sql . "<br>" . $mysqli->error;
+        }  
+    } 
+    else{
+        $id = $_GET["id"];
+        echo $sql = "DELETE FROM `dr_schedule` WHERE id = $id";
+        if ($mysqli->query($sql) === TRUE) {
+            header("Location: view.php");
+        } else {
+            echo "Error: " . $sql . "<br>" . $mysqli->error;
+        }  
+
+    }
+    $mysqli->close();
